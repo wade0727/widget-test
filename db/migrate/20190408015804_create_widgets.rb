0b3b0cff367b4448c039a0b0cc0b3c29f52660bb
Matt <mdwade@gmail.com>
2019-04-18 23:10:22 -0500

    removed widget added product

--- db/migrate/20190408015804_create_widgets.rb
+++
@@ -1,9 +0,0 @@
-class CreateWidgets < ActiveRecord::Migration[5.2]
-  def change
-    create_table :widgets do |t|
-      t.string :widget
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_18_164017) do
+ActiveRecord::Schema.define(version: 2019_04_19_010325) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "thing"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "widgets", force: :cascade do |t|
     t.string "widget"
